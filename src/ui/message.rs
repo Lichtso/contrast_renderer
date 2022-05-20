a091b97416f1d64258756e6add582b82ff8c981a
Alexander Meißner <AlexanderMeissner@gmx.net>
2022-05-20 10:16:04 +0200

    Adds position tracking for speech_balloon.

--- src/ui/message.rs
+++ src/ui/message.rs
@@ -297,6 +297,16 @@ pub const SCROLL_TO: MessengerBehavior = MessengerBehavior {
     reset_at_node_edge: RESET_AT_NODE_EDGE,
 };
 
+/// Send when a property of a node changes
+pub const PROPERTY_CHANGED: MessengerBehavior = MessengerBehavior {
+    label: "PropertyChanged",
+    default_propagation_direction: PropagationDirection::None,
+    get_captured_observable: GET_CAPTURED_OBSERVABLE,
+    do_reflect: DO_REFLECT,
+    update_at_node_edge: UPDATE_AT_NODE_EDGE,
+    reset_at_node_edge: RESET_AT_NODE_EDGE,
+};
+
 #[cfg(feature = "winit")]
 pub struct WinitEventTranslator {
     pub viewport_size: ppga2d::Point,
