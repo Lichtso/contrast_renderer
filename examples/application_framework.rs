2ff21cff84767ee05787606c0ef2ad1d0ec93c44
Alexander Meißner <AlexanderMeissner@gmx.net>
2024-01-24 14:09:36 +0100

    Bumps wgpu to 0.19.1 and winit to 0.29.10.

--- examples/application_framework.rs
+++ examples/application_framework.rs
@@ -43,9 +43,6 @@ impl<'a> Spawner<'a> {
 }
 
 #[cfg(target_arch = "wasm32")]
-use winit::platform::web::WindowExtWebSys;
-
-#[cfg(target_arch = "wasm32")]
 pub struct Spawner {}
 
 #[cfg(target_arch = "wasm32")]
@@ -70,10 +67,10 @@ pub trait Application {
 }
 
 pub struct ApplicationManager {
-    window: winit::window::Window,
+    window: std::sync::Arc<winit::window::Window>,
     instance: wgpu::Instance,
     size: winit::dpi::PhysicalSize<u32>,
-    surface: wgpu::Surface,
+    surface: wgpu::Surface<'static>,
     adapter: wgpu::Adapter,
     device: wgpu::Device,
     queue: wgpu::Queue,
@@ -81,7 +78,7 @@ pub struct ApplicationManager {
 
 impl ApplicationManager {
     pub fn run<A: 'static + Application>(title: &'static str) {
-        let event_loop = winit::event_loop::EventLoop::new();
+        let event_loop = winit::event_loop::EventLoop::new().unwrap();
 
         #[cfg(not(target_arch = "wasm32"))]
         {
@@ -101,30 +98,32 @@ impl ApplicationManager {
     async fn setup(event_loop: &winit::event_loop::EventLoop<()>, title: &'static str) -> Self {
         let mut builder = winit::window::WindowBuilder::new();
         builder = builder.with_title(title);
-        let window = builder.build(event_loop).unwrap();
+        #[cfg(target_arch = "wasm32")]
+        {
+            console_log::init().expect("Could not initialize logger");
+            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+            use wasm_bindgen::JsCast;
+            use winit::platform::web::WindowBuilderExtWebSys;
+            let canvas = web_sys::window()
+                .unwrap()
+                .document()
+                .unwrap()
+                .get_element_by_id("canvas")
+                .unwrap()
+                .dyn_into::<web_sys::HtmlCanvasElement>()
+                .unwrap();
+            builder = builder.with_canvas(Some(canvas));
+        }
+        let window = std::sync::Arc::new(builder.build(event_loop).unwrap());
 
         #[cfg(not(target_arch = "wasm32"))]
         log::set_logger(&LOGGER)
             .map(|()| log::set_max_level(log::LevelFilter::Info))
             .expect("Could not initialize logger");
 
-        #[cfg(target_arch = "wasm32")]
-        {
-            console_log::init().expect("Could not initialize logger");
-            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-            web_sys::window()
-                .and_then(|window| window.document())
-                .and_then(|document| document.body())
-                .and_then(|body| body.append_child(&web_sys::Element::from(window.canvas())).ok())
-                .expect("Couldn't append canvas to document body");
-        }
-
         let instance = wgpu::Instance::default();
-        let (size, surface) = unsafe {
-            let size = window.inner_size();
-            let surface = instance.create_surface(&window).expect("WebGPU is not supported or not enabled");
-            (size, surface)
-        };
+        let size = window.inner_size();
+        let surface = instance.create_surface(window.clone()).expect("WebGPU is not supported or not enabled");
         let adapter = instance
             .request_adapter(&wgpu::RequestAdapterOptions {
                 power_preference: wgpu::PowerPreference::default(),
@@ -152,8 +151,8 @@ impl ApplicationManager {
             .request_device(
                 &wgpu::DeviceDescriptor {
                     label: None,
-                    features: required_features | optional_features,
-                    limits: required_limits,
+                    required_features: required_features | optional_features,
+                    required_limits,
                 },
                 None,
             )
@@ -180,6 +179,7 @@ impl ApplicationManager {
             height: self.size.height,
             present_mode: wgpu::PresentMode::Fifo, // self.surface.get_supported_present_modes(&self.adapter)[0],
             alpha_mode: wgpu::CompositeAlphaMode::Opaque, // self.surface.get_supported_alpha_modes(&adapter)[0],
+            desired_maximum_frame_latency: 2,
         }
     }
 
@@ -210,74 +210,64 @@ impl ApplicationManager {
         let mut average_window_slot = 0;
 
         let spawner = Spawner::new();
-        event_loop.run(move |event, _, control_flow| {
+        let _ = event_loop.run(move |event, target| {
             let _ = (&self.instance, &self.adapter);
-            *control_flow = winit::event_loop::ControlFlow::Wait;
             match event {
-                winit::event::Event::RedrawEventsCleared => {
+                winit::event::Event::AboutToWait => {
                     spawner.run_until_stalled();
-                }
-                winit::event::Event::WindowEvent {
-                    event: winit::event::WindowEvent::Resized(new_inner_size),
-                    ..
-                } => {
-                    self.resize(&mut application, new_inner_size);
-                }
-                winit::event::Event::WindowEvent {
-                    event: winit::event::WindowEvent::ScaleFactorChanged { new_inner_size, .. },
-                    ..
-                } => {
-                    self.resize(&mut application, *new_inner_size);
                 }
                 winit::event::Event::WindowEvent { event, .. } => match event {
                     winit::event::WindowEvent::KeyboardInput {
-                        input:
-                            winit::event::KeyboardInput {
-                                virtual_keycode: Some(winit::event::VirtualKeyCode::Escape),
+                        event:
+                            winit::event::KeyEvent {
+                                logical_key: winit::keyboard::Key::Named(winit::keyboard::NamedKey::Escape),
                                 state: winit::event::ElementState::Pressed,
                                 ..
                             },
                         ..
                     }
                     | winit::event::WindowEvent::CloseRequested => {
-                        *control_flow = winit::event_loop::ControlFlow::Exit;
+                        target.exit();
+                    }
+                    winit::event::WindowEvent::Resized(new_inner_size) => {
+                        self.resize(&mut application, new_inner_size);
+                    }
+                    winit::event::WindowEvent::RedrawRequested => {
+                        #[cfg(not(target_arch = "wasm32"))]
+                        let (animation_time, frame_time) = {
+                            let now = std::time::Instant::now();
+                            let animation_time = (now - start_time).as_secs_f64();
+                            let frame_time = (now - prev_frame).subsec_micros();
+                            prev_frame = now;
+                            (animation_time, frame_time)
+                        };
+                        #[cfg(target_arch = "wasm32")]
+                        let (animation_time, frame_time) = {
+                            let now = clock.now();
+                            let animation_time = (now - start_time) * 0.001;
+                            let frame_time = ((now - prev_frame) * 1000.0) as u32;
+                            prev_frame = now;
+                            (animation_time, frame_time)
+                        };
+                        rolling_average -= average_window[average_window_slot];
+                        average_window[average_window_slot] = frame_time;
+                        rolling_average += average_window[average_window_slot];
+                        average_window_slot = (average_window_slot + 1) % average_window.len();
+                        log::info!(
+                            "rolling_average={} frame_time={}",
+                            rolling_average / average_window.len() as u32,
+                            frame_time
+                        );
+
+                        let frame = self.surface.get_current_texture().unwrap();
+                        application.render(&self.device, &mut self.queue, &frame, animation_time);
+                        frame.present();
                     }
                     _ => {
                         application.window_event(event);
                         self.window.request_redraw();
                     }
                 },
-                winit::event::Event::RedrawRequested(_) => {
-                    #[cfg(not(target_arch = "wasm32"))]
-                    let (animation_time, frame_time) = {
-                        let now = std::time::Instant::now();
-                        let animation_time = (now - start_time).as_secs_f64();
-                        let frame_time = (now - prev_frame).subsec_micros();
-                        prev_frame = now;
-                        (animation_time, frame_time)
-                    };
-                    #[cfg(target_arch = "wasm32")]
-                    let (animation_time, frame_time) = {
-                        let now = clock.now();
-                        let animation_time = (now - start_time) * 0.001;
-                        let frame_time = ((now - prev_frame) * 1000.0) as u32;
-                        prev_frame = now;
-                        (animation_time, frame_time)
-                    };
-                    rolling_average -= average_window[average_window_slot];
-                    average_window[average_window_slot] = frame_time;
-                    rolling_average += average_window[average_window_slot];
-                    average_window_slot = (average_window_slot + 1) % average_window.len();
-                    log::info!(
-                        "rolling_average={} frame_time={}",
-                        rolling_average / average_window.len() as u32,
-                        frame_time
-                    );
-
-                    let frame = self.surface.get_current_texture().unwrap();
-                    application.render(&self.device, &mut self.queue, &frame, animation_time);
-                    frame.present();
-                }
                 _ => {}
             }
         });
