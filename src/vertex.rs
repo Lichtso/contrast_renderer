a189d64a13cc5018864e4d4c60e3341954521316
Alexander Meißner <AlexanderMeissner@gmx.net>
2025-04-26 19:09:42 +0200

    cargo clippy --fix

--- src/vertex.rs
+++ src/vertex.rs
@@ -2,27 +2,27 @@ pub type Vertex0 = [f32; 2];
 
 #[allow(dead_code)]
 #[derive(Clone, Copy)]
-#[repr(packed)]
+#[repr(C, packed)]
 pub struct Vertex2f(pub [f32; 2], pub [f32; 2]);
 
 #[allow(dead_code)]
 #[derive(Clone, Copy)]
-#[repr(packed)]
+#[repr(C, packed)]
 pub struct Vertex2f1i(pub [f32; 2], pub [f32; 2], pub u32);
 
 #[allow(dead_code)]
 #[derive(Clone, Copy)]
-#[repr(packed)]
+#[repr(C, packed)]
 pub struct Vertex3f(pub [f32; 2], pub [f32; 3]);
 
 #[allow(dead_code)]
 #[derive(Clone, Copy)]
-#[repr(packed)]
+#[repr(C, packed)]
 pub struct Vertex3f1i(pub [f32; 2], pub [f32; 3], pub u32);
 
 #[allow(dead_code)]
 #[derive(Clone, Copy)]
-#[repr(packed)]
+#[repr(C, packed)]
 pub struct Vertex4f(pub [f32; 2], pub [f32; 4]);
 
 pub fn triangle_fan_to_strip<T: Copy>(vertices: Vec<T>) -> Vec<T> {
